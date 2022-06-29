700f45e4a04f91811bbb2c27e132cd65251e645c
Monica Dujunco <nievesmonicapd@gmail.com>
2022-06-29 10:22:41 -0500

    msg

--- cat-apult/ViewController.swift
+++ cat-apult/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var insertName: UITextField!
+    @IBOutlet weak var catText: UILabel!
+    @IBOutlet weak var map: UIButton!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func submitName(_ sender: UIButton)
+    {
+        catText.isHidden = false
+        
+        if let usersName = insertName.text
+        {
+            catText.text = "Hello \(usersName)! Welcome to CATAPULT! Ready for your next adventure?"
+        }
+    }
+    
+    @IBAction func nextText(_ sender: Any)
+    {
+        catText.text = "Click the globe above to begin your journey!"
+        map.isHidden = false
+    }
+    
+    
 }
 
